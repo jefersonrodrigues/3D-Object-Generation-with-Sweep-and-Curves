d894c7bda7e5227ad050cdc61c49226bd10e6976
unknown <jeferson.silva@ecomp.ufsm.br>
2022-07-10 01:38:43 -0300

    Criaçao do diretório e organização básica da API

---
+++ README.txt
@@ -0,0 +1,26 @@
+// Jeferson Rodrigues Silva
+// jeferson.silva@ecomp.ufsm.br
+
+Objetivos:
+Explorar transformações geométricas, curvas, modelagem 3D, projeção em perspectiva,
+movimentação de câmera, visualização 3D.
+
+Descrição:
+Implemente um programa para fazer modelagem e visualização 3D de objetos gerados sweep rotacional e curvas de Bezier.
+O usuário deve informar uma sequência de 4 pontos de controle (ou mais). A partir desses pontos, deve-se gerar uma curva de Bezier (com N pontos amostrados) que deve ser rotacionada (sweep rotacional) para geração do objeto tridimensional.
+
+Requisitos básicos (Max: 9.0)
+[] Modelagem do objeto com opção de rotação e translação, para geração de objetos como
+garrafas, donuts e molas
+[] Visualização ortográfica e perspectiva (sob vários ângulos) em wireframe, com uso de
+triângulos
+[] Parametrização do número de pontos e faces do objeto gerado.
+
+Avançados:
+[] Exibir vetores normais em cada face (até 1 ponto)
+[] Adição de mais de 4 pontos de controle (até 1 ponto)
+[] Adição de mais de um patch (até 1 ponto)
+[] Remoção de faces ocultas – sem preenchimento (até 2 pontos)
+[] Preenchimento de polígonos (uma das duas abaixo)
+	[] com Iluminação por vértice (até 4 pontos)
+    []com Iluminação por pixel (até 5 pontos)
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+/*********************************************************************
+// Canvas2D
+// Autor: Cesar Tadeu Pozzer
+//        04/2021
+// *********************************************************************/
+
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "gl_canvas2d.h"
+
+
+int mx, my; //coordenadas do mouse
+
+
+//funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
+//globais que podem ser setadas pelo metodo keyboard()
+void render()
+{
+    color(4);
+    circleFill( 100, 100, 40, 20);
+    circleFill( 160, 160, 30, 5);
+}
+
+//funcao para tratamento de mouse: cliques, movimentos e arrastos
+void mouse(int button, int state, int wheel, int direction, int x, int y)
+{
+   printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+
+   mx = x; //guarda as coordenadas do mouse para exibir dentro da render()
+   my = y;
+}
+
+//funcao chamada toda vez que uma tecla for pressionada
+void keyboard(int key)
+{
+}
+
+
+//funcao chamada toda vez que uma tecla for liberada
+void keyboardUp(int key)
+{
+}
+
+
+int main(void)
+{
+   int screenWidth = 1024, screenHeight = 768; //largura e altura inicial da tela. Alteram com o redimensionamento de tela.
+   init(&screenWidth, &screenHeight, "Trabalho 3 - Jeferson Rodrigues Silva");
+   run();
+}
